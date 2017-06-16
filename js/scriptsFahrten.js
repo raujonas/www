7508ff67c5d8120ba2f349c52e4ac4b27a88095c
Posti90 <sulzerdo@googlemail.com>
2017-06-16 12:55:34 +0200

    fahrt

--- js/scriptsFahrten.js
+++ js/scriptsFahrten.js
@@ -15,6 +15,20 @@ function alleFahrtenAnzeigen(tx, results){
       $('#belegeuebersicht').listview('refresh');             //FEHLER???????????
     }          
 } 
-//API_KEY=
+//API_KEY=AIzaSyDuGWHnapuM2Q4o2PLnL4457wZdJ1r-ZyA
 //Entfernung und Dauer Beispiel von Google (https://developers.google.com/maps/documentation/distance-matrix/start?hl=de) 
 //https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=New+York+City,NY&key=YOUR_API_KEY
+
+function abfrageDistanceAPI(){
+alert("kilometer");
+var request = new XMLHttpRequest();
+    request.open("GET","https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=Washington,DC&destinations=New+York+City,NY&key=AIzaSyDuGWHnapuM2Q4o2PLnL4457wZdJ1r-ZyA");
+    request.addEventListener('load', function(event) {
+       if (request.status >= 200 && request.status < 300) {
+          console.log(request.responseText);
+       } else {
+          console.warn(request.statusText, request.responseText);
+       }
+    });
+    request.send();
+}
