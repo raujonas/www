8a35f8301e96ad0808cad16ce45f88373004de46
Posti90 <sulzerdo@googlemail.com>
2017-06-16 15:02:32 +0200

    ...

--- js/gps.js
+++ js/gps.js
@@ -6,25 +6,25 @@ function onDeviceReady() {
 }
 
 function standortGPS(){ 
+    //Zeitbeschränkung der Standorterfassung 
     var options = {
       enableHighAccuracy: true,
       maximumAge: 3600000
     }
-
 
     // onSuccess Callback
     // This method accepts a Position object, which contains the
     // current GPS coordinates    
     
     function onSuccess(position) {
-      alert( 'Latitude: '          + position.coords.latitude          + '\n' +
-             'Longitude: '         + position.coords.longitude         + '\n' +
-             'Altitude: '          + position.coords.altitude          + '\n' +
-             'Accuracy: '          + position.coords.accuracy          + '\n' +
-             'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-             'Heading: '           + position.coords.heading           + '\n' +
-             'Speed: '             + position.coords.speed             + '\n' +
-             'Timestamp: '         + position.timestamp                + '\n');
+      console.log( 'Latitude: '          + position.coords.latitude          + '\n' +
+                   'Longitude: '         + position.coords.longitude         + '\n' +
+                   'Altitude: '          + position.coords.altitude          + '\n' +
+                   'Accuracy: '          + position.coords.accuracy          + '\n' +
+                   'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+                   'Heading: '           + position.coords.heading           + '\n' +
+                   'Speed: '             + position.coords.speed             + '\n' +
+                   'Timestamp: '         + position.timestamp                + '\n');
    };
 
     // onError Callback receives a PositionError object
